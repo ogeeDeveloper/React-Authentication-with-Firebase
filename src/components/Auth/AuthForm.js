8c8b364af4fcd0ee9b76f3269325215dc0f93b66
ogeeDeveloper <omarogrant202@hotmail.com>
2022-01-26 09:45:48 -0500

    Added user sign up

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,91 @@
+import { useState, useRef } from "react";
+
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // Extract data from form
+    const userEnteredEmail = emailInputRef.current.value;
+    const userEnteredPasswword = passwordInputRef.current.value;
+
+    // TODO: Add validations
+
+    //Checks if mode is login else do the login logics
+    if (isLogin) {
+    } else {
+      // else do logics for user signup
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCmrboXaN6nI0e9r_gT_HTwvrkNEuwwQJU",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: userEnteredEmail,
+            password: userEnteredPasswword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((res) => {
+        if (res.ok) {
+          // TODO: Seds back some information
+        } else {
+          return res.json().then((data) => {
+            // Show Error
+            let errorMessage = "Your Authenticationn failed";
+            // Checks if data have an
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+              console.log(errorMessage);
+            }
+          });
+        }
+      });
+    }
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" ref={emailInputRef} required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            ref={passwordInputRef}
+            required
+          />
+        </div>
+        <div className={classes.actions}>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
